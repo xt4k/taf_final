582f0bdb9cbea65d8ea674fb6e01fd8d9c8b58b9
Oleg <oleg.kh81@gmail.com>
2019-04-12 19:50:56 +0300

    framework -> disable alert notifications for chrome

--- src/main/java/com/academy/framework/BaseTest.java
+++ src/main/java/com/academy/framework/BaseTest.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 
@@ -11,6 +12,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -42,7 +45,13 @@ public class BaseTest {
         switch (browser) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.driver"));
-                driver = new ChromeDriver();
+                // disable notifications
+                Map<String, Object> prefs = new HashMap<String, Object>();
+                prefs.put("profile.default_content_setting_values.notifications", 2);
+//                prefs.put("intl.accept_languages", "en,en_US");
+                ChromeOptions options = new ChromeOptions();
+                options.setExperimentalOption("prefs", prefs);
+                driver = new ChromeDriver(options);
                 break;
 
             case "firefox":
